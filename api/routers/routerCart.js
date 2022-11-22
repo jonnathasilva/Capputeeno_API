66adabfe0335347187a8950006d5dab2bc9fa3e2
jonnathan silva <sjonnathan928@gmail.com>
2022-11-22 14:18:41 -0300

    config deploy

--- api/routers/routerCart.js
+++ api/routers/routerCart.js
@@ -5,7 +5,9 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/cartall", async (req, res) => {
-  const cart = await Cart.find().populate("product").sort({ _id: -1 });
+  const cart = await Cart.findOne({ userId: "1" })
+    .populate("product")
+    .sort({ _id: -1 });
 
   return res.status(200).json(cart);
 });
@@ -28,9 +30,7 @@ router.post("/new", async (req, res) => {
   const existProductByCart = await Cart.findOne({ product: id });
 
   if (existProductByCart) {
-    return res
-      .status(422)
-      .json({ message: "Produto já esta no nosso banco de dados" });
+    return res.status(422).json({ message: "Produto já esta no carrinho" });
   }
 
   const getcart = await Cart.findOne({ userId: "1" });
@@ -42,7 +42,7 @@ router.post("/new", async (req, res) => {
     return res.status(200).json(getcart);
   }
 
-  const cart = await Cart.create({ product: id });
+  const cart = await Cart.create({ product: id, userId: "1" });
 
   return res.status(200).json(cart);
 });
@@ -50,19 +50,33 @@ router.post("/new", async (req, res) => {
 router.delete("/delete", async (req, res) => {
   const { id } = req.body;
 
-  console.log(req.body);
-
-  const cart = await Cart.findOne({ product: id });
+  const cart = await Cart.findOne({ userId: "1" });
 
   if (!cart) {
-    return res
-      .status(422)
-      .json({ message: "Produto não foi encontrado no nosso banco de dados" });
+    return res.status(422).json({ message: "Carrinho não foi encontrado!" });
   }
 
-  const deleteCart = await Cart.findByIdAndRemove(cart._id);
+  if (!id || id === " ") {
+    return res.status(422).json({ message: "Id não foi encontrado!" });
+  }
 
-  return res.status(200).json(deleteCart);
+  const product = cart.product.find((e) => e._id == id);
+
+  if (!product) {
+    return res.status(422).json({ message: "Produto não foi encontrado!" });
+  }
+
+  const update = cart.product.filter((e) => e._id != id);
+
+  cart.product = [];
+
+  update.map((item) => {
+    cart.product.push(item);
+  });
+
+  cart.save();
+
+  return res.status(200).json(cart);
 });
 
 export default router;
